aab5ab16aa3d59b0b921540cccf64540abc89f06
Matt Williamson <matt@teltechsys.com>
2010-10-13 10:51:02 -0400

    Updated to GMaps v3

--- maps.google.circleoverlay.js
+++ maps.google.circleoverlay.js
@@ -5,7 +5,8 @@
 // If you include the library at http://appdelegateinc.com/blog/2010/05/16/point-in-polygon-checking/ you can also check to see if a point resides within the circle.
 
 // Constructor
-var CircleOverlay = function(latLng, radius, strokeColor, strokeWidth, strokeOpacity, fillColor, fillOpacity, numPoints) {
+var CircleOverlay = function(map, latLng, radius, strokeColor, strokeWidth, strokeOpacity, fillColor, fillOpacity, numPoints) {
+	this.map = map;
 	this.latLng = latLng;
 	this.radius = radius;
 	this.strokeColor = strokeColor;
@@ -16,29 +17,24 @@ var CircleOverlay = function(latLng, radius, strokeColor, strokeWidth, strokeOpa
 	
 	// Set resolution of polygon
 	if (typeof(numPoints) == 'undefined') {
-		this.numPoints = 40
+		this.numPoints = 45;
 	} else {
 		this.numPoints = numPoints;
 	}
 }
 
 // Inherit from GOverlay
-CircleOverlay.prototype = GOverlay;
-
-// GMaps initialize callback
-CircleOverlay.prototype.initialize = function(map) {
-	this.map = map;
-}
+CircleOverlay.prototype = new google.maps.OverlayView();
 
 // Reset overlay
 CircleOverlay.prototype.clear = function() {
 	if(this.polygon != null && this.map != null) {
-		this.map.removeOverlay(this.polygon);
+		this.polygon.setMap(null);
 	}
 }
 
 // Calculate all the points of the circle and draw them
-CircleOverlay.prototype.redraw = function(force) {
+CircleOverlay.prototype.draw = function(force) {
 	var d2r = Math.PI / 180;
 	circleLatLngs = new Array();
 	
@@ -52,13 +48,20 @@ CircleOverlay.prototype.redraw = function(force) {
 		var theta = Math.PI * (i / (this.numPoints / 2)); 
 		var vertexLat = this.latLng.lat() + (circleLat * Math.sin(theta)); 
 		var vertexLng = this.latLng.lng() + (circleLng * Math.cos(theta));
-		var vertextLatLng = new GLatLng(vertexLat, vertexLng);
+		var vertextLatLng = new google.maps.LatLng(vertexLat, vertexLng);
 		circleLatLngs.push(vertextLatLng); 
 	}
 	
 	this.clear();
-	this.polygon = new GPolygon(circleLatLngs, this.strokeColor, this.strokeWidth, this.strokeOpacity, this.fillColor, this.fillOpacity);
-	this.map.addOverlay(this.polygon);
+	this.polygon = new google.maps.Polygon({
+		paths: circleLatLngs, 
+		strokeColor: this.strokeColor, 
+		strokeWeight: this.strokeWidth, 
+		strokeOpacity: this.strokeOpacity, 
+		fillColor: this.fillColor, 
+		fillOpacity: this.fillOpacity
+	});
+	this.polygon.setMap(this.map);
 }
 
 // Remove circle method
